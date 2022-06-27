7922c556e81d5096d30b94aa2e152d19b37453c9
julia <agramartinsj@gmail.com>
2022-06-27 20:11:57 -0300

    changed textarea to span for auto resizing

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const inputArea = document.querySelector("#receivedText")
-const textarea = document.querySelector("textarea")
+// const textarea = document.querySelector("textarea")
+const spanInput = document.querySelector("#spanInput")
 
-
+const numbersList = []
 
 function addNumber() {
     const receivedText = inputArea.value
-    //Verificar se é um numero e se nao esta vazio
 
     if (isNaN(receivedText) || receivedText == '') {
         //not a number or empty
         alert("Please write a number")
     } else {
         console.log(receivedText)
-        textarea.innerHTML = `New number added: ${receivedText}` + "\r\n"
+        numbersList[numbersList.length] = receivedText
+        console.log(numbersList)
+        spanInput.innerHTML += `New number added: ${receivedText}` + "</br>"
     }
 }
 
